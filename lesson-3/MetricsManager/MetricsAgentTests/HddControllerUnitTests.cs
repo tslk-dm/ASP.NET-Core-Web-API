980e58052076a30359ad791b1a665147abce86e9
tslk_dm <tislyukdmitriy@mail.ru>
2021-04-03 14:28:07 +0300

    Failed to load NLog LoggingConfiguration.

---
+++ lesson-3/MetricsManager/MetricsAgentTests/HddControllerUnitTests.cs
@@ -0,0 +1,27 @@
+﻿using MetricsAgent.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Xunit;
+
+namespace MetricsAgentTests
+{
+    public class HddControllerUnitTests
+    {
+        private HddMetricsController _controller;
+        public HddControllerUnitTests()
+        {
+            _controller = new HddMetricsController();
+        }
+
+        [Fact]
+        public void GetMetricsFromAgent_ReturnsOk()
+        {
+            //Arrange
+
+            //Act
+            var result = _controller.GetMetricsFromAgent();
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+    }
+}
